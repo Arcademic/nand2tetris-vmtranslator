ac07f3e1bc0698f527ca80c268c4187bf7f03490
Arcademic <Chavoush.k@gmail.com>
2024-05-19 20:40:11 +0200

    Refactored path handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,25 @@
 #include "parser.cpp"
 #include "translator.cpp"
 #include <fstream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 std::string input_type = ".vm";
 std::string output_type = ".asm";
 
 int main(int argc, char *argv[])
 {
-    std::string path = argv[1];
+    fs::path path = argv[1];
+    std::string output_file_path = path.parent_path().string();
+    output_file_path += fs::path::preferred_separator;
+    std::string output_file_name = path.stem().string();
+
     Parser p(path, input_type, output_type);
     std::stringstream cleaned_file = p.cleaned();
-
-    // works only with forward slash for now
-    std::string output_file_path =
-        path.substr(0, path.find_last_of("/") + 1);
-    std::string output_file_name =
-        path.substr(
-                path.find_last_of("/") + 1,
-                path.length() - path.find_last_of("/") - input_type.length() - 1);
-            
     
     Translator t(cleaned_file.str(), output_file_name);
+
     std::ofstream output_file(
         output_file_path
             .append(output_file_name)
