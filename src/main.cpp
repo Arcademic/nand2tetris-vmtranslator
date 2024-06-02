1e5db9fa7750f2b6ff9767dc8596077e856b54f3
Arcademic <Chavoush.k@gmail.com>
2024-06-02 14:45:40 +0200

    handle directory as input

--- src/main.cpp
+++ src/main.cpp
@@ -11,21 +11,51 @@ std::string output_type = ".asm";
 int main(int argc, char *argv[])
 {
     fs::path path = argv[1];
+    if (path.string().back() == '/') {
+        path = path.string().substr(0, path.string().size() - 1);
+    }
     std::string output_file_path = path.parent_path().string();
-    output_file_path += fs::path::preferred_separator;
     std::string output_file_name = path.stem().string();
 
-    Parser p(path, input_type, output_type);
-    std::stringstream cleaned_file = p.cleaned();
-    
-    Translator t(cleaned_file.str(), output_file_name);
-
-    std::ofstream output_file(
+    if (fs::is_directory(path)) {
+        std::clog << "Input is a directory." << std::endl;
         output_file_path
+            .append(1, fs::path::preferred_separator)
             .append(output_file_name)
-            .append(output_type));
-    output_file << t.translated();
-    output_file.close();
+            .append(1, fs::path::preferred_separator)
+            .append(output_file_name)
+            .append(output_type);
+        std::ofstream output_file(output_file_path);
+                    
+        for (const auto& entry : fs::directory_iterator(path)) {
+            if (entry.path().extension() == input_type) {
+                std::clog << "Found VM file: " << entry.path().filename() << std::endl;
+                Parser p(entry.path(), input_type, output_type);
+                std::stringstream cleaned_file = p.cleaned();
+                
+                Translator t(cleaned_file.str(), entry.path().stem().string());
+
+                output_file << t.translated();
+            }
+        }
+        output_file.close();
+        std::clog << "Output file: " << output_file_path << std::endl;
+    } else {
+        std::clog << "Input is a single file." << std::endl;
+        Parser p(path, input_type, output_type);
+        std::stringstream cleaned_file = p.cleaned();
+        
+        Translator t(cleaned_file.str(), output_file_name);
+
+        output_file_path
+                .append(1, fs::path::preferred_separator)
+                .append(output_file_name)
+                .append(output_type);
+        std::ofstream output_file(output_file_path);
+        output_file << t.translated();
+        output_file.close();
+        std::clog << "Output file: " << output_file_path << std::endl;
+    }
 
     return 0;
 }
